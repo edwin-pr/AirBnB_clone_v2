13b6ed1188aa382988f18ad804e7d047ed7d9685
edwin-pr <edwingitau100@gmail.com>
2023-11-22 10:59:18 -0800

    “updates”

--- models/review.py
+++ models/review.py
@@ -1,29 +1,21 @@
 #!/usr/bin/python3
-"""
-Module containing the Review class.
-"""
-
-from sqlalchemy.ext.declarative import declarative_base
+""" Review module for the HBNB project """
+from os import getenv
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, Integer, String, ForeignKey, Float
+from sqlalchemy import Column, String, ForeignKey
+
 
 class Review(BaseModel, Base):
-    """
-    The Review class represents a review in the application.
-
-    Attributes:
-        __tablename__ (str): Represents the table name, which is 'reviews'.
-        text (str): Represents a column containing a string (1024 characters).
-                    It can't be null.
-        place_id (str): Represents a column containing a string (60 characters)
-                        and serves as a foreign key to the 'places.id' column.
-                        It can't be null.
-        user_id (str): Represents a column containing a string (60 characters)
-                       and serves as a foreign key to the 'users.id' column.
-                       It can't be null.
-    """
+    """ Review classto store review information """
 
     __tablename__ = "reviews"
-    text = Column(String(1024), nullable=False)
-    place_id = Column(String(60), ForeignKey("places.id"), nullable=False)
-    user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
+
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+        place_id = Column(String(60), ForeignKey("places.id"), nullable=False)
+        user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
+        text = Column(String(1024), nullable=False)
+
+    else:
+        place_id = ""
+        user_id = ""
+        text = ""
